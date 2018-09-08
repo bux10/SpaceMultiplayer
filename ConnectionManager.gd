68be3eaedab5b4dfd89682340015d00e6388d735
Dustin Moore <dmoore@frostbytecafe.com>
2018-09-07 20:55:14 -0600

    adding world/OSX export

--- ConnectionManager.gd
+++ ConnectionManager.gd
@@ -3,40 +3,55 @@ extends Node
 const PORT = 1234
 var ship = null
 var players = []
+var map
 
 func _ready():
- ship = preload("res://Tank.tscn")
- get_tree().connect("connected_to_server", self, "_connected_ok")
+	ship = preload("res://Tank.tscn")
+	map = preload("res://Maps/Map01.tscn")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
 
 func on_host_game():
- var host = NetworkedMultiplayerENet.new()
- # 4 is the number of maximum clients
- host.create_server(PORT, 4)
- get_tree().set_network_peer(host)
- 
- _connected_ok()
+	var host = NetworkedMultiplayerENet.new()
+	# 4 is the number of maximum clients
+	host.create_server(PORT, 4)
+	get_tree().set_network_peer(host)
+	
+	_connected_ok()
  
 func on_join_game(ip):
- var host = NetworkedMultiplayerENet.new()
- host.create_client(ip, PORT)
- get_tree().set_network_peer(host)
+	var host = NetworkedMultiplayerENet.new()
+	host.create_client(ip, PORT)
+	get_tree().set_network_peer(host)
 
 
 func _connected_ok():
- rpc("register_player", get_tree().get_network_unique_id())
- register_player(get_tree().get_network_unique_id())
- get_tree().get_root().get_node("Lobby").queue_free()
+	rpc("register_player", get_tree().get_network_unique_id())
+	register_player(get_tree().get_network_unique_id())
+	get_tree().get_root().get_node("Lobby").queue_free()
  
 # this function is called when a new player is connected
 # note the use of the keyword remote which mean that the code will only be called on the others
 remote func register_player(player_id):
- var p = ship.instance()
- p.set_network_master(player_id)
- p.name = str(player_id)
- get_tree().get_root().add_child(p)
- # if I'm the server I inform the new connected player about the others
- if get_tree().get_network_unique_id() == 1:
-  if player_id != 1:
-   for i in players:
-    rpc_id(player_id, "register_player", i)
-  players.append(player_id)
+	var p = ship.instance()
+	p.set_network_master(player_id)
+	p.name = str(player_id)
+	#m.set_network_master(player_id)
+	if player_id == 1:
+		var m = map.instance()
+#		var team1 = Node2D.new()
+#		get_tree().get_root().add_child(team1)
+#		get_tree().get_root().add_child(m)
+		get_tree().get_root().add_child(p)
+		print(get_tree().get_root().get_children())
+	else:
+#		var team2 = Node2D.new()
+#		get_tree().get_root().add_child(team2)
+#		get_tree().get_root().get_node("team2").add_child(p)
+		get_tree().get_root().add_child(p)
+		print(get_tree().get_root().get_children())
+	# if I'm the server I inform the new connected player about the others
+	if get_tree().get_network_unique_id() == 1:
+		if player_id != 1:
+			for i in players:
+				rpc_id(player_id, "register_player", i)
+		players.append(player_id)
--- Tank.gd
+++ Tank.gd
@@ -1,12 +1,25 @@
 extends KinematicBody2D
 
-var speed = 200
-var rotation_speed = 80
-var max_speed = 150
-var velocity = Vector2()
+signal shoot
+signal health_changed
+signal dead
+
+export (int) var speed = 200
+export (int) var rotation_speed = 5
+export (int) var max_speed = 150
+export (float) var max_health = 500
+export (Vector2) var velocity = Vector2()
+export (PackedScene) var Bullet
+export (float) var gun_cooldown = 0.2
 
 slave var slave_position = Vector2()
 slave var slave_rotation = 0
+slave var slave_turret_rotation = 0
+
+var can_shoot = true
+var alive = true
+var health
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -20,26 +33,54 @@ func _process(delta):
 	else:
 		position = slave_position
 		rotation = slave_rotation
+		$Turret.rotation = slave_turret_rotation
 
 
 
 func control(delta):
-	look_at(get_global_mouse_position())
-#	var rot_dir = 0
-#	if Input.is_action_pressed('turn_right'):
-#		rot_dir += 1
-#	if Input.is_action_pressed('turn_left'):
-#		rot_dir -= 1
-#	rotation += rotation_speed * rot_dir * delta
+	$Turret.look_at(get_global_mouse_position())
+	var rot_dir = 0
+	if Input.is_action_pressed('turn_right'):
+		rot_dir += 1
+	if Input.is_action_pressed('turn_left'):
+		rot_dir -= 1
+	rotation += rotation_speed * rot_dir * delta
 	velocity = Vector2()
 	if Input.is_action_pressed('forward'):
 		velocity += Vector2(max_speed, 0).rotated(rotation)
-#	if Input.is_action_pressed('back'):
-#		velocity += Vector2(-max_speed, 0).rotated(rotation)
-#		velocity /= 2.0
+	if Input.is_action_pressed('backward'):
+		velocity += Vector2(-max_speed, 0).rotated(rotation)
+		velocity /= 2.0
 #	if Input.is_action_just_pressed('click'):
 #		shoot(gun_shots, gun_spread)
 	position.x = clamp(position.x, $Camera2D.limit_left, $Camera2D.limit_right)
 	position.y = clamp(position.y, $Camera2D.limit_top, $Camera2D.limit_bottom)
 	rset_unreliable("slave_position", position)
 	rset_unreliable("slave_rotation", rotation)
+	rset_unreliable("slave_turret_rotation", $Turret.rotation)
+	
+#func shoot(num, spread, target=null):
+#	if can_shoot:
+#		can_shoot = false
+#		$GunTimer.start()
+#		var dir = Vector2(1, 0).rotated($Turret.global_rotation)
+#		if num > 1:
+#			for i in range(num):
+#				var a = -spread + i * (2*spread)/(num-1)
+#				emit_signal('shoot', Bullet, $Turret/Muzzle.global_position, dir.rotated(a), target)
+#		else:
+#			emit_signal('shoot', Bullet, $Turret/Muzzle.global_position, dir, target)
+#		$AnimationPlayer.play('muzzle_flash')
+#
+#
+#
+#func take_damage(amount):
+#	health -= amount
+#	emit_signal('health_changed', health * 100/max_health)
+#	if health <= 0:
+#		explode()
+#
+#func heal(amount):
+#	health += amount
+#	health = clamp(health, 0, max_health)
+#	emit_signal('health_changed', health * 100/max_health)
