a47e23e3b64acabd157356ddd61619b09c003b0b
Dustin Moore <dmoore@frostbytecafe.com>
2018-09-10 22:00:25 -0600

    animation kinda working

--- tanks/Player.gd
+++ tanks/Player.gd
@@ -13,28 +13,28 @@ func control(delta):
 	$Turret.look_at(get_global_mouse_position())
 	var rot_dir = 0
 	if Input.is_action_pressed('turn_right'):
-		$anim.play('forward')
 		rot_dir += 1
 	if Input.is_action_pressed('turn_left'):
-#		$anim.play_backwards('forward')
 		rot_dir -= 1
 	rotation += rotation_speed * rot_dir * delta
 	velocity = Vector2()
 	if Input.is_action_pressed('forward'):
-		$anim.play('forward')
 		velocity += Vector2(max_speed, 0).rotated(rotation)
 	if Input.is_action_pressed('backward'):
-#		$anim.play_backwards('forward')
 		velocity += Vector2(-max_speed, 0).rotated(rotation)
 		velocity /= 2.0
-	if Input.is_action_just_released('turn_right') or Input.is_action_just_released('turn_left') or Input.is_action_just_released('forward') or Input.is_action_just_released('backward'):
-		$anim.play('idle')
-		pass
 		
 #	if Input.is_action_pressed('shoot'):
 #		rpc("master_shoot", gun_shots, gun_spread)
 #		shoot(gun_shots, gun_spread)
 
+	if Input.is_action_just_pressed('turn_right') or Input.is_action_just_pressed('turn_left') or Input.is_action_just_pressed('forward'):
+		$anim.play('forward')
+	if Input.is_action_just_pressed('backward'):
+		$anim.play_backwards('forward')
+	if Input.is_action_just_released('turn_right') or Input.is_action_just_released('turn_left') or Input.is_action_just_released('forward') or Input.is_action_just_released('backward'):
+		$anim.play('idle')
+	
 	position.x = clamp(position.x, $Camera2D.limit_left, $Camera2D.limit_right)
 	position.y = clamp(position.y, $Camera2D.limit_top, $Camera2D.limit_bottom)
 	rset_unreliable("slave_position", position)
